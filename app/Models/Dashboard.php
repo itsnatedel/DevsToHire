5946b0212dad1aed6407cb4043b599648ab9cf63
Nathan D <nathandeltour2@gmail.com>
2021-09-02 01:09:28 +0200

    Added pics & Dashboard Model

---
+++ app/Models/Dashboard.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dashboard extends Model
+{
+    use HasFactory;
+
+    public static function getUserSettings(User $user)
+    {
+        //dd($user->role_id);
+        dd($user);
+        $user->stats = $user->role_id === 2
+            ? Freelancer::where('user_id', $user->id)->first()
+            : Company::where('user_id', $user->id)->first();
+        $free = Freelancer::where('user_id', $user->id)->first();
+    }
+}
