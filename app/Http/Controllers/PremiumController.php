226c83d60af5e4950093a8fe9ed4f929e91d0d88
mvker <nathandeltour2@gmail.com>
2021-10-10 02:29:19 +0200

    Fixed critical stripe bugs

--- app/Http/Controllers/PremiumController.php
+++ app/Http/Controllers/PremiumController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\DB;
 
 class PremiumController extends Controller
@@ -9,7 +12,7 @@ class PremiumController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @return Application|Factory|View
      */
     public function index()
     {
